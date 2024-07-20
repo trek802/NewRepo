c159c936d519acb95dedd6d2b123c9e8d68e904f
trek802 <arshiya6747@gmail.com>
2024-07-20 11:55:31 +0530

    commit by curr guy

---
+++ selenium-learn/src/main/java/test/programs/NewClass.java
@@ -0,0 +1,7 @@
+package test.programs;
+
+public class NewClass {
+public static void main(String[] args) {
+	System.out.println("New class added by Current guy");
+}
+}
--- selenium-learn/src/main/java/test/programs/Practise.java
+++ selenium-learn/src/main/java/test/programs/Practise.java
@@ -3,7 +3,7 @@ package test.programs;
 public class Practise{
 		
 public static void main(String[] args) {
-	
+	System.out.println("New line added by current guy");
 
 	}
 }
