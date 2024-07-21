2ba4c2c3a16e5b1dbce304a9992781b7aa2e5f66
trek802 <arshiya6747@gmail.com>
2024-07-21 19:34:48 +0530

    commit b4 merge to main

---
+++ selenium-learn/src/main/java/test/programs/DevlpNewClass.java
@@ -0,0 +1,10 @@
+package test.programs;
+
+import org.testng.annotations.Test;
+
+public class DevlpNewClass {
+	@Test
+	void devlpNewClass() {
+		System.out.println("New class added To DEvelop branch");
+	}
+}
